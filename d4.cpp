97251f91163b11fe2c6319fdcb2b8a076247a887
esd1769 <126pubgwarrior@gmail.com>
2025-07-09 13:26:49 -0700

    Update d4.cpp

--- d4.cpp
+++ d4.cpp
@@ -1,20 +1,19 @@
 import java.util.*;
 
-public class ass1 {
+public class ass1{
 
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int min=sc.nextInt();
 
         //int min=3456789;
-        int minutesInDay = 60 * 24;
-        int minutesInYear = minutesInDay * 365;
+        int minutestoDay=60 *24;
+        int minutestoYear=minutestoDay*365;
 
-        int years = min/ minutesInYear;
-        int days = (min% minutesInYear)/minutesInDay;
+        int years= min/minutestoYear;
+        int days=(min% minutestoYear)/minutestoDay;
 
 
-        System.out.println(min+ " minutes is approximately " + years + " years and " + days + " days");
+        System.out.println(min+" minutes is approximately " + years+ " years and " +days+" days");
     }
 }
-
