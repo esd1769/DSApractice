e92e7736e549ea07b26f24de3cd555cf09600e95
esd1769 <126pubgwarrior@gmail.com>
2025-07-21 09:48:36 -0700

    Create d10.cpp

---
+++ d10.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        
+        ListNode* hare=head;
+        ListNode* tortoise=head;
+        while(hare!=NULL&&hare->next!= NULL){
+            tortoise=tortoise->next;
+            hare=hare->next->next;
+        }
+        return tortoise;
+    }
+};
